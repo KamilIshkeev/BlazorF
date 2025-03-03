f03dd0f70f10752142b25471d6f0099133aac236
222209 <ishkeevkamil@gmail.com>
2025-03-03 13:09:52 +0300

    sa

--- BlazorF/ApiFilm/Models/Message.cs
+++ BlazorF/ApiFilm/Models/Message.cs
@@ -10,4 +10,15 @@
         public int? MovieId { get; set; } // ID фильма (может быть null)
         public string? Url { get; set; }
     }
+
+    public class LsMessage
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } // Текст сообщения
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int UserId1 { get; set; } // ID пользователя
+
+        public int UserId2 { get; set; } // ID пользователя
+        public string? Url { get; set; }
+    }
 }
